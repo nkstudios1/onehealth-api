b0449ab78989882c1ec1a876f972e3e1884cb794
deji12 <adesolaayodeji53@gmail.com>
2026-09-17 15:06:39 +0100

    setup static files

--- onehealth/settings.py
+++ onehealth/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/6.1/ref/settings/
 from pathlib import Path
 from decouple import config
 from datetime import timedelta
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -127,6 +128,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = (
+    (os.path.join(BASE_DIR, 'static')),
+)
+STATIC_ROOT = os.path.join(BASE_DIR,  'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Email
